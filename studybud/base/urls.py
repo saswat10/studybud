a1f4b6c91c233b3a59ac4ba79b43f8dce15f64e0
Saswat Samal <saswat1091@outlook.com>
2024-03-01 23:38:15 +0530

    update room view added

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("room_page/<str:id>/", views.room, name="room"),
-    path("create-room", views.createRoom, name="create_room"),
+    path("create-room", views.createRoom, name="create-room"),
+    path("update-room/<str:id>/", views.updateRoom, name="update-room"),
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,25 +4,39 @@ from .forms import RoomForm
 
 # Create your views here.
 
+
 def home(request):
     rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, id):
     room = Room.objects.get(id=id)
-    context = {'room': room}
+    context = {"room": room}
     return render(request, "base/room.html", context)
 
 
 def createRoom(request):
     form = RoomForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect("home")
 
-    context = {'form':form}
-    return render(request, 'base/room_form.html', context)
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+def updateRoom(request, id):
+    room = Room.objects.get(id=id)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
