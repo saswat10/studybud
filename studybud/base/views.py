04ad2a9c0050077eead5f6e965c997e09e4193ce
Saswat Samal <saswat1091@outlook.com>
2024-03-01 23:08:51 +0530

    created Message, Room and Topic model

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
-
+from .models import Room
 
 # Create your views here.
 
 rooms = [
-    {'id': 1, 'title':"Let's learn Python"},
-    {'id': 2, 'title':"Let's learn C++"},
-    {'id': 3, 'title':"Let's learn Rust"},
-    {'id': 4, 'title':"Let's learn GOLang"},
+    {'id': 1, 'name':"Let's learn Python"},
+    {'id': 2, 'name':"Let's learn C++"},
+    {'id': 3, 'name':"Let's learn Rust"},
+    {'id': 4, 'name':"Let's learn GOLang"},
 ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, id):
-    room = None
-    for i in rooms: 
-        if i['id'] == int(id):
-            room = i
+    room = Room.objects.get(id=id)
     context = {'room': room}
     return render(request, "base/room.html", context)
