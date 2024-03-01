4f6332c6034f33fcb184dfbd73908471edc52091
Saswat Samal <saswat1091@outlook.com>
2024-03-01 20:50:10 +0530

    moved the templates to base.template

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,10 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'title':"Let's learn Python"},
+    {'id': 2, 'title':"Let's learn C++"},
+    {'id': 3, 'title':"Let's learn Rust"},
+    {'id': 4, 'title':"Let's learn GOLang"},
+]
 
 def home(request):
-    return render(request, "home.html")
+    context = {'rooms': rooms}
+    return render(request, "base/home.html", context)
 
 
-def room(request):
-    return render(request, "room.html")
+def room(request, id):
+    room = None
+    for i in rooms: 
+        if i['id'] == int(id):
+            room = i
+    context = {'room': room}
+    return render(request, "base/room.html", context)
