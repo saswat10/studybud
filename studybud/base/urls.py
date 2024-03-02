a519762083207eca5c3a9a1c2cc3ad72a733eec4
Saswat Samal <saswat1091@outlook.com>
2024-03-02 10:29:23 +0530

    added register user view

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutUser, name="logout"),
+    path("register/", views.registerUser, name="register"),
     path("", views.home, name="home"),
     path("room_page/<str:id>/", views.room, name="room"),
     path("create-room", views.createRoom, name="create-room"),
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -12,11 +13,12 @@ from .forms import RoomForm
 
 
 def loginPage(request):
+    page = "login"
     if request.user.is_authenticated:
         redirect("home")
 
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("pass")
 
         try:
@@ -31,13 +33,30 @@ def loginPage(request):
         else:
             messages.error(request, "Username and password does not match")
 
-    context = {}
+    context = {"page": page}
     return render(request, "base/login_register.html", context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect("home")
+
+
+def registerUser(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An error occured during registration")
+    
+    context = {"form": form}
+    return render(request, "base/login_register.html", context)
 
 
 def home(request):
